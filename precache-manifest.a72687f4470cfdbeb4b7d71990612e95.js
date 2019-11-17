68bb496a697a71ecc48e78da25041246fc4a808b
abstraced <antoine.luc@gmail.com>
2019-11-17 13:27:38 +0100

    Updates

---
+++ precache-manifest.a72687f4470cfdbeb4b7d71990612e95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbb357744d6e4d9d2f4290d45a0567ef",
+    "url": "/meetup/index.html"
+  },
+  {
+    "revision": "f32ff1e3191cacdb32b8",
+    "url": "/meetup/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "1d6fc48e26c02b1f3eea",
+    "url": "/meetup/static/js/2.931ae898.chunk.js"
+  },
+  {
+    "revision": "f32ff1e3191cacdb32b8",
+    "url": "/meetup/static/js/main.9cfd5e35.chunk.js"
+  },
+  {
+    "revision": "20f25e677fed82f21ee3",
+    "url": "/meetup/static/js/runtime-main.6c9f04d6.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/meetup/static/media/logo.25bf045c.svg"
+  }
+]);
