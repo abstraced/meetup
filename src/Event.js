77908971d29654ccc39b2221cb0382332c214952
abstraced <antoine.luc@gmail.com>
2019-12-14 12:45:33 +0100

    Update Event.js

--- src/Event.js
+++ src/Event.js
@@ -67,23 +67,3 @@ class Event extends Component {
 
 export default Event;
 
-
-// <div className="date">{this.props.event.local_date}  {this.props.event.local_time}</div>
-//     <div className="what">{this.props.event.name}</div>
-//     <div className="who">gtgtg</div>
-//     <div className="attending">Attending: {this.props.event.yes_rsvp_count}</div>
-
-
-//      { this.state.showDetails?
-//      <div className="extra_infos"> 
-//      <div className="address">{this.props.event.venue.address_1}, {this.props.event.venue.city}, {this.props.event.venue.localized_country_name}</div> 
-//      <div className="description">{this.props.event.description}</div> 
-//      <div className="type_of_event">{this.props.event.visibility}</div> 
-//      <div className="link"> {this.props.event.link} </div> 
-
-
-
-//      <button className="toggleInfos" onClick={() => this.toggleDetails()}> Less details </button> 
-//      </div> : 
-//      <button className="toggleInfos" onClick={() => this.toggleDetails()}> More details </button>
-     
