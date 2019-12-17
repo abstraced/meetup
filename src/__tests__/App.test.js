f06b499092685267eff733f9293034379aaa4755
abstraced <antoine.luc@gmail.com>
2019-12-17 21:44:27 +0100

    bug fixing 4.4

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -39,14 +39,15 @@ describe('<App /> integration', () => {
     CitySearchWrapper.instance().handleItemClicked('value', 1.1, 1.2);
     expect(AppWrapper.instance().updateEvents).toHaveBeenCalledTimes(1);
     expect(AppWrapper.instance().updateEvents).toHaveBeenCalledWith(1.1, 1.2);
-    // AppWrapper.unmount();
+
+    AppWrapper.unmount();
   });
   test('change state after get list of events', async () => {
     const AppWrapper = shallow(<App />);
-    AppWrapper.instance().updateEvents(1.1, 1.2);
+    AppWrapper.instance().updateEvents(1.1, 1.2,5);
     await AppWrapper.update();
     expect(AppWrapper.state('events')).toEqual(mockEvents.events);
-    // AppWrapper.unmount();
+    AppWrapper.unmount();
   });
   
 
@@ -54,7 +55,7 @@ describe('<App /> integration', () => {
     const AppWrapper = mount(<App />);
     AppWrapper.setState({ events: [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }] });
     expect(AppWrapper.find('.Event')).toHaveLength(4);
-    // AppWrapper.unmount();
+    AppWrapper.unmount();
   
   });
 
@@ -63,10 +64,10 @@ describe('<App /> integration', () => {
     AppWrapper.instance().updateEvents = jest.fn();
     AppWrapper.instance().forceUpdate();
     const EventNumberWrapper = AppWrapper.find(EventNumber);
-    EventNumberWrapper.instance().handleValueChange({ target: { value: 1 } });
+    EventNumberWrapper.instance().handleInputChanged({ target: { value: 1 } });
     expect(AppWrapper.instance().updateEvents).toHaveBeenCalledTimes(1);
-    expect(AppWrapper.instance().updateEvents).toHaveBeenCalledWith( 1);
-    // AppWrapper.unmount();
+    expect(AppWrapper.instance().updateEvents).toHaveBeenCalledWith(null, null, 1);
+    AppWrapper.unmount();
   });
 
 });
--- src/__tests__/EventNumber.test.js
+++ src/__tests__/EventNumber.test.js
@@ -12,24 +12,25 @@ describe('<EventNumber /> component', () => {
     });
 
     test('render textbox element', () => {
-      expect(EventNumberWrapper.find('.EventNumber')).toHaveLength(1);
+      expect(EventNumberWrapper.find('.numberOfEvents')).toHaveLength(1);
     });
     
     test('show number of events  label', () => {
-      expect(EventNumberWrapper.find('.EventNumber label')).toHaveLength(1);
+      expect(EventNumberWrapper.find('.numberOfEvents label')).toHaveLength(1);
     });
 
 
     test('render text input', () => {
-        expect(EventNumberWrapper .find('.EventNumber')).toHaveLength(1);
+        expect(EventNumberWrapper .find('.numberOfEvents')).toHaveLength(1);
       });
 
 
-      test('change state when text input changes', () => {
-        const eventObject = { target: { value: 30 }};
-        EventNumberWrapper .find('.event_number_input').simulate('change', eventObject);
-        expect(EventNumberWrapper .state('numberOfEvents')).toBe(30);
+      test('change state when input changes', () => {
+        const eventObject = { target: { value: 24 }};
+        EventNumberWrapper.find('#numberOfEvents__input').simulate('change', eventObject);
+        expect(EventNumberWrapper.state('numberOfEvents')).toBe(24);
       });
+    
 
    
       
